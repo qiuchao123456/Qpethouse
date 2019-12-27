e61fbf36f993582c8a9701c2464c1abd38055e20
1224342739 <1224342739@qq.com>
2019-12-27 14:15:20 +0800

    homepage完善

--- src/Mid2/mid2.js
+++ src/Mid2/mid2.js
@@ -5,7 +5,13 @@ import img3 from '../img/67.jpg';
 import img4 from '../img/68.jpg';
 import img5 from '../img/69.jpg';
 import img6 from '../img/70.jpg';
-import Fenlei from '../Fenlei/fenlei';
+import img7 from '../img/71.jpg';
+import img8 from '../img/72.png';
+import img9 from '../img/73.jpg';
+import img10 from '../img/74.jpg';
+import img11 from '../img/75.png';
+import img12 from '../img/76.png';
+import {Link} from 'react-router-dom';
 
 var Mid2Css =require('./mid2.css');
 
@@ -15,11 +21,87 @@ export default class Mid2 extends React.Component{
         return(
             <div>
                 <div className={Mid2Css.div1}>
+                    <div className={Mid2Css.div8}>
+                        热卖宠物
+                    </div>
+                    <div className={Mid2Css.div3}>
+                        <marquee scrollamount="8">宠物能给我们带来快乐，挑选一只心仪的宠物吧！</marquee>
+                    </div>
+                    <div className={Mid2Css.div4}>
+                        <div className={Mid2Css.div5}>
+                            <Link to="/gou/xiboliyaxueqiaoquan">二哈</Link>
+                        </div>
+                        <div className={Mid2Css.div6}>
+                            <Link to="/gou/xiboliyaxueqiaoquan"><img src={img7}></img></Link>
+                        </div>
+                        <div className={Mid2Css.div7}>
+                            ￥：1200元/只
+                        </div>
+                    </div>
+                    <div className={Mid2Css.div4}>
+                        <div className={Mid2Css.div5}>
+                            <Link to="/mao/buoumao">布偶猫</Link>
+                        </div>
+                        <div className={Mid2Css.div6}>
+                            <Link to="/mao/buoumao"><img src={img8}></img></Link>
+                        </div>
+                        <div className={Mid2Css.div7}>
+                            ￥：2000元/只
+                        </div>
+                    </div>
+                    <div className={Mid2Css.div4}>
+                        <div className={Mid2Css.div5}>
+                            <Link to="/yu/jinyu">金鱼</Link>
+                        </div>
+                        <div className={Mid2Css.div6}>
+                            <Link to="/yu/jinyu"><img src={img9}></img></Link>
+                        </div>
+                        <div className={Mid2Css.div7}>
+                            ￥：5元/只
+                        </div>
+                    </div>
+                    <div className={Mid2Css.clearfix}></div>
+                    <div className={Mid2Css.div4}>
+                        <div className={Mid2Css.div5}>
+                            <Link to="/gui/jinqiangui">金钱龟</Link>
+                        </div>
+                        <div className={Mid2Css.div6}>
+                            <Link to="/gui/jinqiangui"><img src={img10}></img></Link>
+                        </div>
+                        <div className={Mid2Css.div7}>
+                            ￥：100元/只
+                        </div>
+                    </div>
+                    <div className={Mid2Css.div4}>
+                        <div className={Mid2Css.div5}>
+                            <Link to="/tu/daerbaitu">大耳白兔</Link>
+                        </div>
+                        <div className={Mid2Css.div6}>
+                            <Link to="/tu/daerbaitu"><img src={img11}></img></Link>
+                        </div>
+                        <div className={Mid2Css.div7}>
+                            ￥：300元/只
+                        </div>
+                    </div>
+                    <div className={Mid2Css.div4}>
+                        <div className={Mid2Css.div5}>
+                            <Link to="/niao/yingwu">鹦鹉</Link>
+                        </div>
+                        <div className={Mid2Css.div6}>
+                            <Link to="/niao/yingwu"><img src={img12}></img></Link>
+                        </div>
+                        <div className={Mid2Css.div7}>
+                            ￥：200元/斤
+                        </div>
+                    </div>
+
+                    <div className={Mid2Css.clearfix}></div>    
+
                     <div className={Mid2Css.div2}>
                         精品宠物粮
                     </div>
                     <div className={Mid2Css.div3}>
-                        广告
+                    <marquee scrollamount="8">宠物是我们的好朋友，当然要吃最好的粮食，我们提供了优质粮食供参考。</marquee>
                     </div>
                     <div className={Mid2Css.div4}>
                         <div className={Mid2Css.div5}>
@@ -31,6 +113,7 @@ export default class Mid2 extends React.Component{
                         <div className={Mid2Css.div7}>
                             ￥：100元/斤
                         </div>
+                        <button className={Mid2Css.anniu}>添加到购物车</button>
                     </div>
                     <div className={Mid2Css.div4}>
                         <div className={Mid2Css.div5}>
@@ -42,6 +125,7 @@ export default class Mid2 extends React.Component{
                         <div className={Mid2Css.div7}>
                             ￥：100元/斤
                         </div>
+                        <button className={Mid2Css.anniu}>添加到购物车</button>
                     </div>
                     <div className={Mid2Css.div4}>
                         <div className={Mid2Css.div5}>
@@ -53,6 +137,7 @@ export default class Mid2 extends React.Component{
                         <div className={Mid2Css.div7}>
                             ￥：100元/斤
                         </div>
+                        <button className={Mid2Css.anniu}>添加到购物车</button>
                     </div>
                     <div className={Mid2Css.clearfix}></div>
                     <div className={Mid2Css.div4}>
@@ -65,6 +150,7 @@ export default class Mid2 extends React.Component{
                         <div className={Mid2Css.div7}>
                             ￥：100元/斤
                         </div>
+                        <button className={Mid2Css.anniu}>添加到购物车</button>
                     </div>
                     <div className={Mid2Css.div4}>
                         <div className={Mid2Css.div5}>
@@ -76,6 +162,7 @@ export default class Mid2 extends React.Component{
                         <div className={Mid2Css.div7}>
                             ￥：100元/斤
                         </div>
+                        <button className={Mid2Css.anniu}>添加到购物车</button>
                     </div>
                     <div className={Mid2Css.div4}>
                         <div className={Mid2Css.div5}>
@@ -87,6 +174,7 @@ export default class Mid2 extends React.Component{
                         <div className={Mid2Css.div7}>
                             ￥：100元/斤
                         </div>
+                        <button className={Mid2Css.anniu}>添加到购物车</button>
                     </div>  
                 </div>
             </div>
